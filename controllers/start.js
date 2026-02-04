acb8c9948d10042c17ba0d7c0c62d0bb2b06da03
iga s <igasmyrak@gmail.com>
2026-02-04 14:55:14 +0000

    changing again

--- controllers/start.js
+++ controllers/start.js
@@ -5,7 +5,7 @@ import logger from "../utils/logger.js";
 const start = {
     createView(request, response) {
         logger.info("start page loading..");
-        response.send("welcome to iga's playlist app");
+        response.send('welcome to igas playlist app');
     },
 };
 
